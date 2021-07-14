de223802b2b67e0887b2c5d981434a419dbd5311
Praveen <70476863+Praveen606@users.noreply.github.com>
2021-07-14 13:48:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('its a string')
+console.log('in a string we can display anything in javascript')
+var va = 'varble'
+var what = 'in a varble we store anything n a javascript'
+console.log(va)
+console.log(va,what)
+var between = 'state of'
+console.log(`power is a ${between} mind`)
+var age = 126;
+if(age < 18){
+    alert('Does not qualify for driving')
+}else{
+    alert('qualify for driving')
+}
